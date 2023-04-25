cf3aeddae2006556d4cb822f178d95206b4ec942
Alberto Fernández <afernandezgom@gmail.com>
2023-04-25 20:45:12 +0200

    :recycle:  Protect home page route

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,19 +1,35 @@
-import ButtonGroup from "@module/ButtonGroup/ButtonGroup";
+import ButtonGroup from '@module/ButtonGroup/ButtonGroup';
+import { Box } from '@mui/material';
+import { useSession } from 'next-auth/react';
+import Router from 'next/router';
+import { useEffect } from 'react';
 
 const puzzles = [
   {
-    title: "Puzzle 1",
+    title: 'Puzzle 1',
     key: 'puzzle1'
   },
   {
-    title: "Puzzle 2",
+    title: 'Puzzle 2',
     key: 'puzzle2'
   }
 ]
 
 const Home = () => {
+  const { status: sessionStatus } = useSession() as { status: string }
+ 
+  useEffect(() => {
+    if (sessionStatus === 'loading') {
+      return
+    } else if (sessionStatus === 'unauthenticated') {
+      Router.push('/api/auth/signin')
+    }
+  }, [ sessionStatus ])
+
   return (
-    <ButtonGroup puzzles={puzzles} />
+    <Box className='pt-container'>
+      <ButtonGroup puzzles={puzzles} />
+    </Box>
   );
 };
 
