c8666f8a933eb0d7b935283dc80b272cc5c149f9
Alberto Fernández <afernandezgom@gmail.com>
2023-04-25 20:51:39 +0200

    :label:  Add custom types

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,21 @@
 declare module 'config'
 declare module 'bcryptjs'
+declare module 'cookies'
 
 interface PuzzleOption {
   title: string,
   key: string
+}
+
+interface User {
+  username?: string,
+  puzzle1Complete?: boolean,
+  puzzle2Complete?: boolean,
+  password?: string,
+  id?: string,
+  createdAt?: string,
+  updatedAt?: string
+}
+interface UserSession {
+  user: User
 }
