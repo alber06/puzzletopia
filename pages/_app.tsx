3d31d83a289121c9f3c53a3bcd5506f7b5599436
Alberto Fernández <afernandezgom@gmail.com>
2023-04-25 20:58:26 +0200

    :lipstick:  Fix global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
 import MenuBarLayout from '@layout/MenuBarLayout'
 
+import 'styles/global.css'
+
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
