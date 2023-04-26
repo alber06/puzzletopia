415207257bf82e43dc46e546e71a6ecbfc7fc86c
Alberto Fernández <afernandezgom@gmail.com>
2023-04-26 11:24:10 +0200

    :recycle:  Extract success text to it's own component

---
+++ frontend/components/elements/SuccessText.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material"
+
+  const SuccessText = ({ text }: { text: string }) => {
+    return <Typography color='green'>{text}</Typography>
+  }
+
+  export default SuccessText
